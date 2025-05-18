ed36c7e18ca594d69d03dc0fa0d589ffe9f76ac5
cplKlegg <emcgrath@startmail.com>
2025-05-17 20:24:51 -0400

    Mint NFTS Part 1

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,6 +10,8 @@ contract NFT is ERC721Enumerable, Ownable {
     uint256 public allowMintingOn;
     string public baseURI;
 
+    event Mint(uint256 amount, address minter);
+
     constructor(
         string memory _name, 
         string memory _symbol,
@@ -22,6 +24,30 @@ contract NFT is ERC721Enumerable, Ownable {
             maxSupply = _maxSupply;
             allowMintingOn = _allowMintingOn;
             baseURI = _baseURI;
+    }
+
+    function mint(uint256 _mintAmount) public payable {
+        // Only allow minting after specified time
+        require(block.timestamp >= allowMintingOn);
+        // Must mint at least 1 token        
+        require(_mintAmount > 0);        
+        // Require enough payment
+        require(msg.value >= cost * _mintAmount);
+
+        
+        uint256 supply = totalSupply();
+
+        // Do not let them mint more tokens than available
+        require(supply + _mintAmount <= maxSupply);
+        
+        // Create tokens
+        for(uint256 i =1; i<= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+        
+
+        // Emit event
+        emit Mint(_mintAmount, msg.sender);
     }
 
 }
